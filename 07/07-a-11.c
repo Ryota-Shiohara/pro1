0c79c512cd4176fd397040d93dd1d725b1236b00
Ryota-Shiohara <ryotashiohara@keio.jp>
2025-11-13 15:59:31 +0900

    07完成

--- 07/07-a-11.c
+++ 07/07-a-11.c
@@ -1,40 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int isOdd(int);
+#define N 8
+int board[N] = {0};
+int solutions = 1;
 
-int isEven(int n) {
-    if (n == 0) {
-        return 1;
-    } else if (n > 0) {
-        return isOdd(n - 1);
+void solve_queen(int col) {
+    if (col == -1) {
+        int i, j;
+        printf("solution = %d\n", solutions++);
+        for (i = 0; i < N; i++) {
+            for (j = 0; j < N; j++) {
+                if (board[j] == i) {
+                    printf("Q");
+                } else {
+                    printf(".");
+                }
+            }
+            printf("\n");
+        }
+        return;
     } else {
-        return isOdd(n + 1);
-    }
-}
-
-int isOdd(int n) {
-    if (n == 0) {
-        return 0;
-    } else if (n > 0) {
-        return isEven(n - 1);
-    } else {
-        return isEven(n + 1);
+        int row;
+        for (row = 0; row < N; row++) {
+            int i, canPlace = 1;
+            for (i = col + 1; i < N; i++) {
+                if (board[i] == row || abs(board[i] - row) == abs(i - col)) {
+                    canPlace = 0;
+                    break;
+                }
+            }
+            if (canPlace) {
+                board[col] = row;
+                solve_queen(col - 1);
+                board[col] = 0;
+            }
+        }
     }
 }
 
 
 int main() {
-    int n;
-    printf("input an integer: ");
-    scanf("%d", &n);
-    
-    printf("%d is ", n);
-    if (isEven(n)) {
-        printf("even\n");
-    } else {
-        printf("odd\n");
-    }
-
+    solve_queen(N - 1);
     return 0;
 }
