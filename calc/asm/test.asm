9a9b9fe194994dfe77113c695313b20d453a0e97
Ryota-Shiohara <ryotashiohara@keio.jp>
2026-01-24 21:09:00 +0900

    calc: jal用のアセンブリ

--- calc/asm/test.asm
+++ calc/asm/test.asm
@@ -1,6 +1,12 @@
+	# JAL 動作確認: x1 に PC+4 が入ることと、PC が飛ぶことをテスト
+	addi x1,x0,256
+	jal x1,skip
+	sw x0,x1,0
+skip: sw x1,x1,4
+	# ここまで
 	addi x4,x0,0
 	addi x5,x0,9
-    addi x8,x0,2
+	addi x8,x0,2
 	sll x5,x5,x8
 	addi x8,x0,5
 loop: lw x6,x4,0
